f03d4f2a5b2baf3e266c3f31f582ad2ca4d9ddbd
Willian S <wsapcf@gmail.com>
2021-04-19 09:47:16 -0300

    make score card overlay app bar

---
+++ lib/widgets/score_card/score_card.dart
@@ -0,0 +1,50 @@
+import 'package:devquiz/core/core.dart';
+import 'package:devquiz/widgets/widgets.dart';
+import 'package:flutter/material.dart';
+
+class ScoreCard extends StatefulWidget {
+  @override
+  _ScoreCardState createState() => _ScoreCardState();
+}
+
+class _ScoreCardState extends State<ScoreCard> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 20),
+      child: Container(
+          height: 136,
+          decoration: BoxDecoration(
+              color: AppColors.white, borderRadius: BorderRadius.circular(15)),
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Expanded(flex: 1, child: ChartWidget()),
+                Expanded(
+                  flex: 3,
+                  child: Padding(
+                    padding: EdgeInsets.only(left: 24),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "Vamos começar",
+                          style: AppTextStyles.heading,
+                        ),
+                        Text(
+                          "Complete os desafios e avance em conhecimento",
+                          style: AppTextStyles.body,
+                        )
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
+          )),
+    );
+  }
+}
--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -1,1 +1,3 @@
-export 'app_bar.dart';
+export 'app_bar/app_bar.dart';
+export 'chart/chart.dart';
+export 'score_card/score_card.dart';
