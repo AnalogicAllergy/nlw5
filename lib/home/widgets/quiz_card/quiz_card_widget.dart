57aa182dc52394382903210553cbed094a1feb83
Willian S <wsapcf@gmail.com>
2021-04-20 10:32:43 -0300

    create QuizCardWidget setup

--- lib/home/widgets/quiz_card/quiz_card_widget.dart
+++ lib/home/widgets/quiz_card/quiz_card_widget.dart
@@ -1,21 +1,53 @@
+import 'package:devquiz/core/core.dart';
 import 'package:flutter/material.dart';
 
 class QuizCardWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 5,
-      child: Container(
-          height: 175,
-          width: 160,
-          child: Column(
-            children: [
-              //build
-            ],
+    return Container(
+      padding: EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        color: AppColors.white,
+        border: Border.fromBorderSide(
+          BorderSide(color: AppColors.border),
+        ),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            child: Image.asset(AppImages.blocks),
+            height: 40,
+            width: 40,
           ),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(10),
-          )),
+          SizedBox(
+            height: 24,
+          ),
+          Text(
+            "Gerenciamento de Estado",
+            style: AppTextStyles.heading15,
+          ),
+          SizedBox(
+            height: 24,
+          ),
+          Row(
+            children: [
+              Expanded(
+                  flex: 1, child: Text("3 de 10", style: AppTextStyles.body11)),
+              Expanded(
+                flex: 4,
+                child: LinearProgressIndicator(
+                  backgroundColor: AppColors.chartSecondary,
+                  value: 0.3,
+                  valueColor:
+                      AlwaysStoppedAnimation<Color>(AppColors.chartPrimary),
+                ),
+              )
+            ],
+          )
+        ],
+      ),
     );
   }
 }
