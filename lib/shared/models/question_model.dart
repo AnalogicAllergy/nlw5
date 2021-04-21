a29e952e454e82f6d31631f6ef1ac57ff3f74750
Willian S <wsapcf@gmail.com>
2021-04-21 09:41:32 -0300

    make sure QuizModel has Level enumeration

--- lib/shared/models/question_model.dart
+++ lib/shared/models/question_model.dart
@@ -4,5 +4,6 @@ class QuestionModel {
   final String title;
   final List<AnswerModel> answers;
 
-  QuestionModel({required this.title, required this.answers});
+  QuestionModel({required this.title, required this.answers})
+      : assert(answers.length == 4);
 }
--- lib/shared/models/quiz_model.dart
+++ lib/shared/models/quiz_model.dart
@@ -1,8 +1,18 @@
 import 'models.dart';
 
+enum Level { facil, medio, dificil, perito }
+
 class QuizModel {
   final String title;
   final List<QuestionModel> questions;
+  final int questionsAnswered;
+  final String image;
+  final Level level;
 
-  QuizModel({required this.title, required this.questions});
+  QuizModel(
+      {required this.questionsAnswered = 0,
+      required this.image,
+      required this.level,
+      required this.title,
+      required this.questions});
 }
