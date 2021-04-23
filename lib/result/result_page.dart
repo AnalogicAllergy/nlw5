c322416366029b1bedfdee55f389a8aa8a58ae86
Willian S <wsapcf@gmail.com>
2021-04-23 10:34:38 -0300

    add more quizzes to database

--- lib/result/result_page.dart
+++ lib/result/result_page.dart
@@ -2,7 +2,6 @@ import 'package:devquiz/core/core.dart';
 import 'package:devquiz/result/components/back_button.dart';
 import 'package:devquiz/result/components/share_button.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 class ResultPage extends StatelessWidget {
   const ResultPage({
@@ -20,34 +19,32 @@ class ResultPage extends StatelessWidget {
           Center(
             child: Text(
               "Parabéns",
-              style: GoogleFonts.notoSans(
-                fontWeight: FontWeight.w700,
-                fontSize: 30,
-              ),
+              style: AppTextStyles.heading40,
             ),
           ),
           SizedBox(height: 20),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 20),
-            child: RichText(
-              textAlign: TextAlign.center,
-              text: TextSpan(
-                  text: "Você concluiu\n",
-                  style:
-                      AppTextStyles.body.copyWith(fontWeight: FontWeight.w400),
-                  children: [
-                    TextSpan(
-                        text: "Gerenciamento de Estado\n",
-                        style: AppTextStyles.body.copyWith(
-                            fontWeight: FontWeight.w600, fontSize: 16)),
-                    TextSpan(text: " com 6 de 10 acertos")
-                  ]),
-            ),
+          RichText(
+            textAlign: TextAlign.center,
+            text: TextSpan(
+                text: "Você concluiu\n",
+                style: AppTextStyles.body
+                    .copyWith(fontWeight: FontWeight.w400, fontSize: 16),
+                children: [
+                  TextSpan(
+                      text: "Gerenciamento de Estado\n",
+                      style: AppTextStyles.body
+                          .copyWith(fontWeight: FontWeight.w600, fontSize: 16)),
+                  TextSpan(text: " com 6 de 10 acertos")
+                ]),
           ),
           SizedBox(height: 80),
           ShareButton.purple(label: "Compartilhar", onTap: () {}),
           SizedBox(height: 20),
-          BackButtonWidget(onTap: () {}, label: "Voltar ao início")
+          BackButtonWidget(
+              onTap: () {
+                Navigator.pop(context);
+              },
+              label: "Voltar ao início")
         ],
       ),
     );
